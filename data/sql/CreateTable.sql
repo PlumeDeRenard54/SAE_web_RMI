e3f72418f24f25dc5b240aec35e3a4de608612b4
Paul Antoine <e85555@etu.univ-lorraine.fr>
2026-06-01 17:59:58 +0200

    correction sql

--- data/sql/CreateTable.sql
+++ data/sql/CreateTable.sql
@@ -15,28 +15,7 @@ CREATE TABLE Reservation (
     NomCli VARCHAR2(30),
     IdRestaurant INTEGER,
     DateRes DATE,
-/* Restaurant (Id, Nom, Adresse, Lat, Lon) */
-
-CREATE TABLE Restaurant (
-    Id INTEGER PRIMARY KEY,
-    Nom VARCHAR2(30),
-    Adresse VARCHAR2(30),
-    Lat FLOAT,
-    Lon FLOAT
-);
-
-/* Reservation (IdRes, NomCli, IdRestaurant, DateRes)*/
-
-CREATE TABLE Reservation (
-    IdRes INTEGER PRIMARY KEY,
-    NomCli VARCHAR2(30),
-    IdRestaurant INTEGER,
-    DateRes DATE,
     NbTables INTEGER,
-    
-    CONSTRAINT fk_RestoReserv FOREIGN KEY (IdRestaurant) REFERENCES RESTAURANT(Id)
-);
 
-    
     CONSTRAINT fk_RestoReserv FOREIGN KEY (IdRestaurant) REFERENCES RESTAURANT(Id)
 );
