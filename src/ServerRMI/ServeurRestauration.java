cd70d194960f37c7c1cb9d71efe6434290fb4d26
Alexander CARNET <e67005u@univ-lorraine.fr>
2026-06-02 09:25:01 +0200

    [UPDATE] implementation de getRestaurants

--- src/ServerRMI/ServeurRestauration.java
+++ src/ServerRMI/ServeurRestauration.java
@@ -5,6 +5,7 @@ import donnees.Restaurant;
 
 import java.io.Serializable;
 import java.rmi.Remote;
+import java.sql.SQLException;
 import java.util.List;
 
 public class ServeurRestauration implements ServiceRestauration {
@@ -15,7 +16,13 @@ public class ServeurRestauration implements ServiceRestauration {
 
     @Override
     public List<Restaurant> getRestaurants() {
-        return List.of();
+        Repository r = Repository.getInstance();
+        try{
+            return r.getRestaurants();
+        }catch(SQLException e){
+            e.printStackTrace();
+        }
+        return null;
     }
 
 
--- src/donnees/Reservation.java
+++ src/donnees/Reservation.java
@@ -1,9 +1,32 @@
 package donnees;
 
 public class Reservation {
-    private int idReservation;
     private String nomCli;
     private int idRestaurant;
     private String dateReservation;
     private int nbTables;
+
+    public Reservation(String nomCli, int idRestaurant, String dateReservation, int nbTables) {
+        this.nomCli = nomCli;
+        this.idRestaurant = idRestaurant;
+        this.dateReservation = dateReservation;
+        this.nbTables = nbTables;
+    }
+
+
+    public String getNomCli() {
+        return nomCli;
+    }
+
+    public int getIdRestaurant() {
+        return idRestaurant;
+    }
+
+    public String getDateReservation() {
+        return dateReservation;
+    }
+
+    public int getNbTables() {
+        return nbTables;
+    }
 }
