fbf4b67558a8e9691d529924ca60c61607d7a643
Matthieu Petelot <matthieu.petelot5@etu.univ-lorraine.fr>
2026-06-03 09:59:56 +0200

    ajout de l'api java

---
+++ src/APIJava/handlers/GetRestosHandler.java
@@ -0,0 +1,58 @@
+package APIJava.handlers;
+
+import ServerRMI.ServiceRestauration;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import donnees.Restaurant;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.rmi.NotBoundException;
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.util.List;
+
+import static main.Main.sendOptionResponse;
+
+public class GetRestosHandler implements HttpHandler {
+
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+        if ("OPTIONS".equalsIgnoreCase(exchange.getRequestMethod())) {
+            sendOptionResponse(exchange);
+            return;
+        }
+
+        if ("GET".equalsIgnoreCase(exchange.getRequestMethod())) {
+
+            exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+            exchange.getResponseHeaders().add("Content-Type", "application/json");
+//                System.out.println(json);
+            System.out.println("Appel de getRestos");
+            Registry reg = LocateRegistry.getRegistry("10.82.149.19", 1099);
+//                System.out.println(reg);
+            ServiceRestauration resto = null;
+            try {
+                resto = (ServiceRestauration) reg.lookup("serviceBD");
+                System.out.println("ref distante récupérée");
+            } catch (NotBoundException e) {
+                System.out.println(e.getMessage());
+                throw new RuntimeException(e);
+            }
+            List<Restaurant> listeRestos = resto.getRestaurants();
+//                    System.out.println(listeRestos.toString());
+            String json = (new ObjectMapper()).writeValueAsString(listeRestos);
+            System.out.println("Affichage JSON");
+            System.out.println(json);
+            System.out.println("FIn affichage JSON");
+            exchange.sendResponseHeaders(200, json.getBytes().length);
+            OutputStream os = exchange.getResponseBody();
+            os.write(json.getBytes());
+            os.close();
+        }
+
+    }
+}
---
+++ src/APIJava/main/Main.java
@@ -0,0 +1,78 @@
+package APIJava.main;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+import APIJava.handlers.GetAccidentsHandler;
+import APIJava.handlers.GetRestosHandler;
+import APIJava.handlers.GetVelibsHandler;
+import APIJava.handlers.ReservationHandler;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        HttpServer server = HttpServer.create(new InetSocketAddress(8080), 0);
+
+        //gestion de l'adresse http://localhost:8080/showVelib
+        server.createContext("/velib", new GetVelibsHandler());
+
+        //reservation d'une table au resto
+        server.createContext("/reserver", new ReservationHandler());
+
+        //récupération des restos
+        server.createContext("/getRestos", new GetRestosHandler());
+
+        //récupérer les travaux
+        server.createContext("/travaux", new GetAccidentsHandler());
+
+        //gestion d'un simple sout sur un clic, sera remplacé par le rmi j'imagine
+        server.createContext("/soutMessage", new HttpHandler() {
+            @Override
+            public void handle(HttpExchange exchange) throws IOException {
+                exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+                System.out.println("Appel RMI");
+                exchange.sendResponseHeaders(200, 0);
+                exchange.getResponseBody().close();
+
+            }
+        });
+
+
+        server.start();
+        System.out.println("Serveur lancé sur 8080");
+
+    }
+
+
+    public static String getInfosAPI(String uri) throws IOException, InterruptedException {
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create(uri))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+        int statusCode = response.statusCode();
+        String body = response.body();
+//        System.out.println(body);
+        System.out.println("Status de la réponse : " + statusCode);
+        return body;
+    }
+
+    public static void sendOptionResponse(HttpExchange exchange) throws IOException {
+        exchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET, OPTIONS");
+        exchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Content-Type, Authorization");
+        exchange.sendResponseHeaders(204, -1);
+        exchange.close();
+    }
+
+
+}
