7ccdc70c34b9e3967cd1b691e3392d05ef858498
Paul Antoine <e85555@etu.univ-lorraine.fr>
2026-06-03 08:37:18 +0200

    SQL

--- data/sql/CreateTable.sql
+++ data/sql/CreateTable.sql
@@ -1,5 +1,3 @@
-DROP TABLE TABLERESERVATION;
-DROP TABLE TABLES;
 DROP TABLE RESERVATION;
 DROP TABLE RESTAURANT;
 
@@ -9,7 +7,8 @@ CREATE TABLE Restaurant (
                             Nom VARCHAR2(30),
                             Adresse VARCHAR2(100),
                             Lat NUMBER(12,8),
-                            Lon NUMBER(12,8)
+                            Lon NUMBER(12,8),
+                            NbTables INTEGER
 );
 
 CREATE TABLE Reservation (
@@ -24,36 +23,14 @@ CREATE TABLE Reservation (
                              CONSTRAINT fk_RestoReserv FOREIGN KEY (IdRestaurant) REFERENCES RESTAURANT(Id)
 );
 
-CREATE TABLE Tables (
-                        IdTable INTEGER NOT NULL PRIMARY KEY,
-                        NbPlaces INTEGER
-);
-
-CREATE TABLE TableReservation (
-                                  IdRes INTEGER NOT NULL,
-                                  IdTable INTEGER NOT NULL,
-
-                                  CONSTRAINT pk_TableReservation PRIMARY KEY (IdRes, IdTable),
-
-                                  CONSTRAINT fk_TableRes1 FOREIGN KEY (IdRes) REFERENCES Reservation (IdRes),
-                                  CONSTRAINT fk_TableRes2 FOREIGN KEY (IdTable) REFERENCES Tables (IdTable)
-
-);
-
 commit;
 
 
 GRANT ALL PRIVILEGES ON restaurant TO e67005u;
 GRANT ALL PRIVILEGES ON reservation TO e67005u;
-GRANT ALL PRIVILEGES ON tables TO e67005u;
-GRANT ALL PRIVILEGES ON tablereservation TO e67005u;
 
 GRANT ALL PRIVILEGES ON restaurant TO e82916u;
 GRANT ALL PRIVILEGES ON reservation TO e82916u;
-GRANT ALL PRIVILEGES ON tables TO e82916u;
-GRANT ALL PRIVILEGES ON tablereservation TO e82916u;
 
 GRANT ALL PRIVILEGES ON restaurant TO e24838u;
 GRANT ALL PRIVILEGES ON reservation TO e24838u;
-GRANT ALL PRIVILEGES ON tables TO e24838u;
-GRANT ALL PRIVILEGES ON tablereservation TO e24838u;
